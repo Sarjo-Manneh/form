3b8bb0b9b6044fd13f2210e03656bd18d9c82540
Sarjo-Manneh <128193130+Sarjo-Manneh@users.noreply.github.com>
2023-10-17 10:48:18 -0400

    made changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import React from 'react';
 import Validate from "./components/Validate/Validate";
-import Dropdown from "./components/Dropdown/Dropdown";
-import Checkbox from "./components/Checkbox/Checkbox";
-import Multiple from "./components/Multiple/Multiple";
+ import Dropdown from "./components/Dropdown/Dropdown";
+ import Checkbox from "./components/Checkbox/Checkbox";
+ import Multiple from "./components/Multiple/Multiple";
 import Uncontrolled from "./components/Uncontrolled/Uncontrolled";
 import Controlled from "./components/Controlled/Controlled";
 import ReactHookForm from "./components/ReactHookForm/ReactHookForm";
+import Index from "./components/Index/Index"
+
 
 
 const App = () => {
   return (
     <div>
       <Controlled />
-      <Dropdown />
-      <Checkbox />
-      <Multiple />
-      <Validate />
+       <Dropdown />
+       <Checkbox />
+       <Multiple />
+       <Validate />
       <Uncontrolled />
-      <ReactHookForm />
+       <ReactHookForm />    
+       <Index/>
     </div>
   );
 };
---
+++ src/components/Index/Index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Index.css';
+
+export function Index() {
+    return (
+        <div className="index-component">
+            {/* Add content for the Index component here */}
+        </div>
+    );
+}
+
+export function App() {
+    return (
+        <div className="app-container">
+            <Index/>
+        </div>
+    );
+}
